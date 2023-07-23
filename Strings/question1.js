ccc0be9829238484f6539dc35a1ce085573aa513
K Subramanyeshwara <subramanyeshwarak@gmail.com>
2023-07-23 11:59:43 +0530

    Strings question Added

---
+++ Strings/question1.js
@@ -0,0 +1,9 @@
+//What is the length of an empty string?
+const sampleString = "";
+console.log(sampleString.length);//0
+
+//What is the length of a string with single space?
+const spaceString = " ";
+console.log(spaceString.length);//1
+
+
